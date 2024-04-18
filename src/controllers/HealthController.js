4ba0a603fac51e57d35fe72c34e194b4594d5287
Juan Sebastian Sanchez L. <js.sanchezl12@uniandes.edu.co>
2024-04-18 13:56:27 -0500

    Add support for feature branches in deploy workflow and update health controller response message

--- src/controllers/HealthController.js
+++ src/controllers/HealthController.js
@@ -4,7 +4,7 @@ const {constants} = require('http2');
 const healthController = express.Router();
 
 healthController.get("/status", (req, res) => {
-    res.status(constants.HTTP_STATUS_OK).json({ status: "OK", code: constants.HTTP_STATUS_OK});
+    res.status(constants.HTTP_STATUS_OK).json({ status: "OK :)", code: constants.HTTP_STATUS_OK});
 });
 
 module.exports = healthController;
